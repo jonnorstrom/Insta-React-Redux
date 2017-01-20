0990b6fe8a361cd11449e9189be685b10b3194f0
Jonathan Norstrom <jon.norstrom@gmail.com>
2017-01-20 12:09:15 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Insta-WHAT?!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux'
+
+
+// everything here?
+
+
+// const store = createStore(reducer)
+var store;
+
+export default store
